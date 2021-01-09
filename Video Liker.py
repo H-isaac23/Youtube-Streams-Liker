a472a4a7c76b34c38565195d5d950f736c374364
John Isaac Delgado <ji23delgado@gmail.com>
2021-01-09 11:14:23 +0800

    Supplies a dictionary of names and links to like_video func

--- Video Liker.py
+++ Video Liker.py
@@ -3,14 +3,16 @@ from streamActivity import is_streaming
 import time
 
 with open('channel ids.txt', 'r') as f:
+    video_links = {}
     for line in f.readlines():
         channel_id = line[:24]
         channel_link = 'https://www.youtube.com/channel/' + channel_id
 
         # check if streaming
         if is_streaming(channel_link):
-            channel_name = line[24:-1]
+            channel_name = line[27:-1]
             print(channel_name)
+
             # Get the stream link
             video_link = get_stream_link(channel_link)
 
@@ -21,11 +23,13 @@ with open('channel ids.txt', 'r') as f:
             if video_link in link_read.read():
                 print(f'Video from {channel_name} is already liked.')
             else:
-                like_video(video_link)
+                print('Video link added to queue.')
+                video_links[channel_name] = video_link
                 link_write.write(video_link + '\n')
 
-        t = time.localtime()
-        current_time = time.strftime("%H:%M:%S", t)
+    print()
+    like_video(video_links)
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
 
 print(f"Time finished: {current_time}")
-
