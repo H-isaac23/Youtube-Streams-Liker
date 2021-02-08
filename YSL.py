089abebeda013f1c69b392290e087074af305ad2
John Isaac Delgado <ji23delgado@gmail.com>
2021-02-08 17:50:03 +0800

    like_videos func complete

--- YSL.py
+++ YSL.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from collections import OrderedDict
+from random import randint
 from datetime import date
 import requests
 import os
@@ -42,6 +43,9 @@ class StreamLiker(YSL):
         self.stream_data['Time Started'] = self.time_started
 
     def is_streaming(self):
+
+        print('Checking for streams') ###### Status Code
+
         for name in self.channels.keys():
             channel_url = 'https://www.youtube.com/channel/' + self.channels[name]
             response = requests.get(channel_url).text
@@ -69,20 +73,79 @@ class StreamLiker(YSL):
 
                 with open('video links.txt', 'r') as link_read:
                     if link in link_read.read():
-                        print(f'Video from {name} is already liked.')
+                        print(f'Video from {name} is already liked.') ##### Status Code
                     elif link not in link_read.read():
                         self.number_of_to_be_liked_streams += 1
                         self.streams_liked[name] = link
                         self.video_ids.append(link[32:])
                         # num_to_be_liked += 1
                         with open('video links.txt', 'a') as link_write:
-                            # link_write.write(link + '\n')
+                            link_write.write(link + '\n')
                             print(f'Video stream of {name} added to queue')
 
             except:
                 print("XPATH not found")
                 driver.quit()
                 return None
+
+        driver.quit()
+
+    def like_videos(self):
+
+        print("Logging into google...\n") ##### Status Code
+
+        option = FirefoxOptions()
+        option.add_argument('--headless')
+        option.add_argument('--mute-audio')
+        PATH = 'C:/Program Files (x86)/geckodriver.exe'
+        driver = webdriver.Firefox(options=option, executable_path=PATH)
+
+        EMAIL = os.environ.get('TEST_EMAIL')
+        PASSWORD = os.environ.get('TEST_PASS')
+
+        driver.get(
+            'https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAAQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+        try:
+            email = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="identifierId"]'))
+            )
+            time.sleep(randint(4, 7))
+            email.send_keys(EMAIL)
+            email.send_keys(Keys.RETURN)
+        except:
+            print('There is a problem in the email idk lmao, driver quitting')
+            driver.quit()
+
+        time.sleep(5)
+
+        try:
+            password = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH,
+                     "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input"))
+            )
+            time.sleep(3)
+            password.send_keys(PASSWORD)
+            password.send_keys(Keys.RETURN)
+        except:
+            print("Password textbox not found")
+            driver.quit()
+
+        time.sleep(5)
+
+        for name, link in self.streams_liked.items():
+            driver.get(link)
+            try:
+                button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH, '//*[@id="top-level-buttons"]/ytd-toggle-button-renderer[1]/a'))
+                )
+                ActionChains(driver).move_to_element(button).click(button).perform()
+                print(f'Video from {name} successfully liked.')
+                time.sleep(5)
+            except:
+                print('Xpath not found')
+                driver.quit()
 
         driver.quit()
 
@@ -97,4 +160,5 @@ sl.get_channels()
 sl.is_streaming()
 # print(sl.currently_streaming)
 sl.get_stream_links()
+sl.like_videos()
 
