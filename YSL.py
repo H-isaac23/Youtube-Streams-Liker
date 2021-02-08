7c3f8e2359dc9f01a78a3852b5937629e5182ee0
John Isaac Delgado <ji23delgado@gmail.com>
2021-02-08 18:10:45 +0800

    append_data_on_file func complete

--- YSL.py
+++ YSL.py
@@ -8,9 +8,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 from collections import OrderedDict
 from random import randint
 from datetime import date
+import csv
 import requests
 import os
 import time
+
 
 class YSL:
     def __init__(self, channels_path):
@@ -25,6 +27,7 @@ class YSL:
                 channel_name = channel[27:-1]
                 self.channels[channel_name] = channel_id
 
+
 class StreamLiker(YSL):
     def __init__(self, channels_path):
         super(StreamLiker, self).__init__(channels_path)
@@ -36,6 +39,8 @@ class StreamLiker(YSL):
         self.stream_data = OrderedDict()
         self.number_of_active_streams = 0
         self.number_of_to_be_liked_streams = 0
+        self.total_time_elapsed = None
+        self.time_ended = None
 
     def get_start_time(self):
         self.start_time = time.time()
@@ -44,7 +49,8 @@ class StreamLiker(YSL):
 
     def is_streaming(self):
 
-        print('Checking for streams') ###### Status Code
+        ##### Status Code
+        print('Checking for streams')
 
         for name in self.channels.keys():
             channel_url = 'https://www.youtube.com/channel/' + self.channels[name]
@@ -54,12 +60,14 @@ class StreamLiker(YSL):
                 self.currently_streaming[name] = channel_url
                 self.number_of_active_streams += 1
 
+        self.stream_data['No. of active streams'] = self.number_of_active_streams
+
     def get_stream_links(self):
-        PATH = 'C:/Program Files (x86)/geckodriver.exe'
+        path = 'C:/Program Files (x86)/geckodriver.exe'
         options = FirefoxOptions()
         options.add_argument('--headless')
         options.add_argument('--mute-audio')
-        driver = webdriver.Firefox(options=options, executable_path=PATH)
+        driver = webdriver.Firefox(options=options, executable_path=path)
         # num_to_be_liked = 0
 
         for name, channel_link in self.currently_streaming.items():
@@ -73,7 +81,8 @@ class StreamLiker(YSL):
 
                 with open('video links.txt', 'r') as link_read:
                     if link in link_read.read():
-                        print(f'Video from {name} is already liked.') ##### Status Code
+                        ##### Status Code
+                        print(f'Video from {name} is already liked.')
                     elif link not in link_read.read():
                         self.number_of_to_be_liked_streams += 1
                         self.streams_liked[name] = link
@@ -88,66 +97,101 @@ class StreamLiker(YSL):
                 driver.quit()
                 return None
 
+        self.stream_data['No. of to-be-liked streams'] = self.number_of_to_be_liked_streams
+        self.stream_data['Streams liked'] = ', '.join(self.streams_liked.values())
         driver.quit()
 
     def like_videos(self):
 
-        print("Logging into google...\n") ##### Status Code
+        if self.streams_liked == {}:
+            print("All streams on the channels you provided are already liked.")
+        else:
+            ##### Status Code
+            print("Logging into google...\n")
 
-        option = FirefoxOptions()
-        option.add_argument('--headless')
-        option.add_argument('--mute-audio')
-        PATH = 'C:/Program Files (x86)/geckodriver.exe'
-        driver = webdriver.Firefox(options=option, executable_path=PATH)
+            option = FirefoxOptions()
+            option.add_argument('--headless')
+            option.add_argument('--mute-audio')
+            path = 'C:/Program Files (x86)/geckodriver.exe'
+            driver = webdriver.Firefox(options=option, executable_path=path)
 
-        EMAIL = os.environ.get('TEST_EMAIL')
-        PASSWORD = os.environ.get('TEST_PASS')
+            EMAIL = os.environ.get('TEST_EMAIL')
+            PASSWORD = os.environ.get('TEST_PASS')
 
-        driver.get(
-            'https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAAQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
-        try:
-            email = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, '//*[@id="identifierId"]'))
-            )
-            time.sleep(randint(4, 7))
-            email.send_keys(EMAIL)
-            email.send_keys(Keys.RETURN)
-        except:
-            print('There is a problem in the email idk lmao, driver quitting')
-            driver.quit()
-
-        time.sleep(5)
-
-        try:
-            password = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located(
-                    (By.XPATH,
-                     "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input"))
-            )
-            time.sleep(3)
-            password.send_keys(PASSWORD)
-            password.send_keys(Keys.RETURN)
-        except:
-            print("Password textbox not found")
-            driver.quit()
-
-        time.sleep(5)
-
-        for name, link in self.streams_liked.items():
-            driver.get(link)
+            driver.get(
+                'https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAAQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
             try:
-                button = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable(
-                        (By.XPATH, '//*[@id="top-level-buttons"]/ytd-toggle-button-renderer[1]/a'))
+                email = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="identifierId"]'))
                 )
-                ActionChains(driver).move_to_element(button).click(button).perform()
-                print(f'Video from {name} successfully liked.')
-                time.sleep(5)
+                time.sleep(randint(4, 7))
+                email.send_keys(EMAIL)
+                email.send_keys(Keys.RETURN)
             except:
-                print('Xpath not found')
+                print('There is a problem in the email idk lmao, driver quitting')
                 driver.quit()
 
-        driver.quit()
+            time.sleep(5)
+
+            try:
+                password = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH,
+                         "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input"))
+                )
+                time.sleep(3)
+                password.send_keys(PASSWORD)
+                password.send_keys(Keys.RETURN)
+            except:
+                print("Password textbox not found")
+                driver.quit()
+
+            time.sleep(5)
+
+            for name, link in self.streams_liked.items():
+                driver.get(link)
+                try:
+                    button = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable(
+                            (By.XPATH, '//*[@id="top-level-buttons"]/ytd-toggle-button-renderer[1]/a'))
+                    )
+                    ActionChains(driver).move_to_element(button).click(button).perform()
+                    print(f'Video from {name} successfully liked.')
+                    time.sleep(5)
+                except:
+                    print('Xpath not found')
+                    driver.quit()
+
+            driver.quit()
+
+    def get_end_time(self):
+        time_ended = time.strftime("%H:%M:%S", time.localtime())
+        self.stream_data['Time Ended'] = time_ended
+        self.total_time_elapsed = time.time() - self.start_time
+        self.stream_data['Time elapsed'] = self.total_time_elapsed
+
+    def append_data_on_file(self):
+        # File naming
+        today = date.today()
+        d = today.strftime("%m/%d/%y")
+        hyphenated_date = '-'.join(d.split('/'))
+        filename = f'stream_data {hyphenated_date}.csv'
+
+        # Check if a file exists
+        if not os.path.exists(f"Stream data/{filename}"):
+            with open(f"Stream data/{filename}", 'a', newline='') as csv_file:
+                fieldnames = ['Time elapsed', 'No. of active streams', 'No. of to-be-liked streams', 'Time Started',
+                              'Time Ended',
+                              'Streams liked']
+                csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                csv_writer.writeheader()
+
+        with open(f"Stream data/{filename}", 'a', newline='') as csv_file:
+            fieldnames = ['Time elapsed', 'No. of active streams', 'No. of to-be-liked streams', 'Time Started',
+                          'Time Ended',
+                          'Streams liked']
+            csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            csv_writer.writerow(self.stream_data)
 
 
 # ysl = YSL('channel ids.txt')
@@ -156,9 +200,10 @@ class StreamLiker(YSL):
 
 sl = StreamLiker('channel ids.txt')
 sl.get_channels()
-# sl.get_start_time()
+sl.get_start_time()
 sl.is_streaming()
 # print(sl.currently_streaming)
 sl.get_stream_links()
 sl.like_videos()
-
+sl.get_end_time()
+sl.append_data_on_file()
