8745020b6631bca0ad216e8b3260f0ce075e7da8
John Isaac Delgado <ji23delgado@gmail.com>
2021-02-08 17:27:34 +0800

    get_stream_func in progress

--- YSL.py
+++ YSL.py
@@ -1,8 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from collections import OrderedDict
+from datetime import date
 import requests
 import os
-from datetime import date
 import time
-from collections import OrderedDict
 
 class YSL:
     def __init__(self, channels_path):
@@ -23,6 +30,7 @@ class StreamLiker(YSL):
         self.start_time = None
         self.time_started = None
         self.currently_streaming = {}
+        self.streams_liked = []
         self.stream_data = OrderedDict()
         self.number_of_active_streams = 0
         self.number_of_to_be_liked_streams = 0
@@ -39,6 +47,44 @@ class StreamLiker(YSL):
             stream_active = '{"text":" watching"}' in response
             if stream_active:
                 self.currently_streaming[name] = channel_url
+                self.number_of_active_streams += 1
+
+    def get_stream_links(self):
+        PATH = 'C:/Program Files (x86)/geckodriver.exe'
+        options = FirefoxOptions()
+        options.add_argument('--headless')
+        options.add_argument('--mute-audio')
+        driver = webdriver.Firefox(options=options, executable_path=PATH)
+        # num_to_be_liked = 0
+
+        for name, channel_link in self.currently_streaming.items():
+            driver.get(channel_link + '/videos')
+
+            try:
+                video_url = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="thumbnail"]'))
+                )
+                link = video_url.get_attribute('href')
+                with open('video links.txt', 'r') as link_read:
+                    if link in link_read.read():
+                        print(f'Video from {name} is already liked.')
+                    elif link not in link_read.read():
+                        self.number_of_to_be_liked_streams += 1
+                        # num_to_be_liked += 1
+                        with open('video links.txt', 'a') as link_write:
+                            self.streams_liked.append(link)
+                            link_write.write(link + '\n')
+                            print(f'Video stream of {name} added to queue')
+
+            except:
+                print("XPATH not found")
+                driver.quit()
+                return None
+
+        print(self.streams_liked)
+        print(self.number_of_to_be_liked_streams)
+
+        driver.quit()
 
 
 # ysl = YSL('channel ids.txt')
@@ -47,7 +93,8 @@ class StreamLiker(YSL):
 
 sl = StreamLiker('channel ids.txt')
 sl.get_channels()
-sl.get_start_time()
+# sl.get_start_time()
 sl.is_streaming()
-print(sl.currently_streaming)
+# print(sl.currently_streaming)
+sl.get_stream_links()
 
