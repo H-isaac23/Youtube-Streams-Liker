31e8a844f20ea4dbdc182808e10b950099d3a91f
John Isaac Delgado <ji23delgado@gmail.com>
2021-02-20 15:38:54 +0800

    make a web server for remote control

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from YSL import StreamLiker
+import os
+
+email = os.environ.get('TEST_EMAIL')
+passwd = os.environ.get('TEST_PASS')
+sl = StreamLiker('channel ids.txt', email, passwd)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/like')
+def like():
+    sl.start_liking_with_data("isaac", "localhost", "DevAisha23!", "YSL")
+    return render_template('like.html')
+
+if __name__ == '__main__':
+    print("Hi")
+    app.run(debug=True, host='192.168.1.10')
