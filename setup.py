01b20de6fc1d6c3a1ecd9294350793085d58afaa
John Isaac Delgado <ji23delgado@gmail.com>
2021-02-25 07:47:27 +0800

    v1.2.0 update

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="YSL-H-isaac23",
-    version="1.1.4",
+    version="1.2.0",
     author="John Isaac Delgado",
     author_email="dev.isaac23@gmail.com",
-    description="A package for automated liking of Active YouTube Streams in the background using selenium and requests.",
+    description="A package for automated liking of Active YouTube Streams programmatically using selenium and requests.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT',
